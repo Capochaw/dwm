bea8581eaf65be2ea1c9fbc6e9d6ff4a6b0fd11f
Capocha <d4rkarok@gmail.com>
2025-02-13 21:03:43 +0000

    Add files via upload

---
+++ patch/include.c
@@ -0,0 +1,31 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
+#include "bar_systray.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "cfacts.c"
+#include "dragcfact.c"
+#include "floatpos.c"
+#include "mpdcontrol.c"
+#include "movestack.c"
+#include "restartsig.c"
+#include "sticky.c"
+#include "swallow.c"
+#include "togglefullscreen.c"
+#include "xrdb.c"
+#include "dragmfact.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "cfacts.h"
+#include "dragcfact.h"
+#include "dragmfact.h"
+#include "floatpos.h"
+#include "mpdcontrol.h"
+#include "movestack.h"
+#include "restartsig.h"
+#include "sticky.h"
+#include "swallow.h"
+#include "togglefullscreen.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
